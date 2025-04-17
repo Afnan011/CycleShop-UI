12ba9fee74b8919e83649e6a76bccca5ea78a41e
Mahammad Afnan <muhammedafnan8184@gmail.com>
2025-04-17 10:28:34 +0530

    Updated the Employee Add/Edit model for proper sytling
    Code refactor, replaced map-get with map.get and repaced darken with
    scale, adjust

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -5,25 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-modal',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="modal" *ngIf="isOpen">
-      <div class="modal-overlay" (click)="close.emit()"></div>
-      <div class="modal-content">
-        <div class="modal-header">
-          <h2>{{title}}</h2>
-          <button class="close-btn" (click)="close.emit()">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
-        <div class="modal-body">
-          <ng-content></ng-content>
-        </div>
-        <div class="modal-footer">
-          <ng-content select="[modal-footer]"></ng-content>
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
